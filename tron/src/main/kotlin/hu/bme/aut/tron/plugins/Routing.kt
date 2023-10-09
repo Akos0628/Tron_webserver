d903fb8df34992defed13c7a1e80287142c1a108
Akos0628 <75949273+Akos0628@users.noreply.github.com>
2023-10-09 18:14:25 +0200

    Testing auto deploy with a nice message :)

--- tron/src/main/kotlin/hu/bme/aut/tron/plugins/Routing.kt
+++ tron/src/main/kotlin/hu/bme/aut/tron/plugins/Routing.kt
@@ -7,7 +7,15 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Hail, stranger, who seeks to play our wondrous Tron game.\n" +
+                    "\n" +
+                    "Welcome to our humble kingdom, where we play a game of skill and speed.\n" +
+                    "\n" +
+                    "The goal of the game is to be the first player to reach the finish line.\n" +
+                    "\n" +
+                    "You will need to use your reflexes and agility to avoid your opponents' light cycles.\n" +
+                    "\n" +
+                    "If you are brave enough to challenge us, we will be glad to play.")
         }
     }
 }
