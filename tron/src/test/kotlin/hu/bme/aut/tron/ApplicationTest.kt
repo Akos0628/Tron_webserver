36b071a2d856174894e5c1edbb7ffa0806ed19f5
Akos0628 <75949273+Akos0628@users.noreply.github.com>
2023-10-09 18:19:36 +0200

    Fixed tests

--- tron/src/test/kotlin/hu/bme/aut/tron/ApplicationTest.kt
+++ tron/src/test/kotlin/hu/bme/aut/tron/ApplicationTest.kt
@@ -15,7 +15,15 @@ class ApplicationTest {
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals("Hail, stranger, who seeks to play our wondrous Tron game.\n" +
+                    "\n" +
+                    "Welcome to our humble kingdom, where we play a game of skill and speed.\n" +
+                    "\n" +
+                    "The goal of the game is to be the first player to reach the finish line.\n" +
+                    "\n" +
+                    "You will need to use your reflexes and agility to avoid your opponents' light cycles.\n" +
+                    "\n" +
+                    "If you are brave enough to challenge us, we will be glad to play.", bodyAsText())
         }
     }
 }
