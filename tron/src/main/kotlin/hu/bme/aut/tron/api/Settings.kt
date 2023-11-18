fa67da36b4c6b9f32d78a540f0daf9f9a430755d
Akos0628 <75949273+Akos0628@users.noreply.github.com>
2023-11-18 23:38:51 +0100

    minor changes

--- tron/src/main/kotlin/hu/bme/aut/tron/api/Settings.kt
+++ tron/src/main/kotlin/hu/bme/aut/tron/api/Settings.kt
@@ -4,7 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Settings(
-    val timeLimit: Long? = null,
     val turnTimeLimit: Long,
     val playerLimit: Int,
     val bots: List<RobotSettings>
