d1da4baa7b3ec55a692ee457de411d07171ed840
Akos0628 <75949273+Akos0628@users.noreply.github.com>
2023-11-23 22:29:39 +0100

    get funky

--- docker_service/BikeService/app/app.py
+++ docker_service/BikeService/app/app.py
@@ -9,8 +9,10 @@ app = Flask(__name__)
 nnbike = NNBike.NNBike()
 nnbike.load_model('model_jo.h5')
 
-qbike = QBike.QBike()
-qbike.load_model("QLearningV5.Qtable")
+qbikeV5 = QBike.QBike()
+qbikeV5.load_model("QLearningV5.Qtable")
+qbikeV2 = QBike.QBike()
+qbikeV2.load_model("QLearningV2-Funky.Qtable")
 
 @app.route('/')
 def hello_world():
@@ -42,9 +44,21 @@ def qstep(map):
     step = qbike.step(submap)
     return str(step)
 
+@app.route('/qstep_funky/<map>')
+def qstep(map):
+    #convert the map to a list of ints
+    map = map.split(',')
+    submap = np.array([int(map[0]), int(map[1]), int(map[2]),
+                       int(map[3]), 0, int(map[5]),
+                       int(map[6]), int(map[7]), int(map[8])])
+
+    #get the next step
+    step = qbike.step(submap)
+    return str(step)
+
 @app.route('/get_bike_models')
 def get_bike_models():
-    return ["nnstep", "qstep"]
+    return ["nnstep", "qstep", "qstep_funky"]
 
 @app.route('/health')
 def health():
